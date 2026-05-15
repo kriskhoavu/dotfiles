6e55f5fe9b8fddedae19c125d3e81a904eadb21a
Khoa Vu <khoavu.its@gmail.com>
2026-05-15 17:53:43 +0700

    feat: Focus editing window when launching fzf from NeoTree avoid buffer jumping

--- nvim/lua/plugins/fzf-lua.lua
+++ nvim/lua/plugins/fzf-lua.lua
@@ -37,27 +37,49 @@ local function with_neotree_directory(opts)
     return vim.tbl_extend("force", opts or {}, { cwd = directory })
 end
 
+-- When launching fzf from NeoTree, switch to an editing window first
+-- so the selected file opens in the right place
+local function focus_edit_window()
+    if vim.bo.filetype ~= "neo-tree" then return end
+
+    local cur_win = vim.api.nvim_get_current_win()
+    for _, w in ipairs(vim.api.nvim_tabpage_list_wins(0)) do
+        if w ~= cur_win then
+            local cfg = vim.api.nvim_win_get_config(w)
+            local buf = vim.api.nvim_win_get_buf(w)
+            if cfg.relative == "" and vim.bo[buf].filetype ~= "neo-tree" then
+                vim.api.nvim_set_current_win(w)
+                return
+            end
+        end
+    end
+end
+
 return {
     "ibhagwan/fzf-lua",
     keys = {
-        { "<leader>ff", function() require("fzf-lua").files(with_neotree_directory()) end, desc = "Find Files" },
-        { "<leader>ft", function() require("fzf-lua").live_grep(with_neotree_directory()) end, desc = "Live Grep" },
-        { "<leader>fg", function() require("fzf-lua").git_files() end, desc = "Find Git Files" },
+        { "<leader>ff", function() local opts = with_neotree_directory(); focus_edit_window(); require("fzf-lua").files(opts) end, desc = "Find Files" },
+        { "<leader>ft", function() local opts = with_neotree_directory(); focus_edit_window(); require("fzf-lua").live_grep(opts) end, desc = "Live Grep" },
+        { "<leader>fg", function() focus_edit_window(); require("fzf-lua").git_files() end, desc = "Find Git Files" },
         {
             "<leader>fG",
             function()
-                require("fzf-lua").live_grep(with_neotree_directory({
+                local opts = with_neotree_directory({
                     rg_opts = "--hidden --glob '!.git/*' --column --line-number --no-heading --color=always -e",
-                }))
+                })
+                focus_edit_window()
+                require("fzf-lua").live_grep(opts)
             end,
             desc = "Live Grep includes hidden files",
         },
-        { "<leader>fb", function() require("fzf-lua").buffers() end, desc = "Buffers" },
-        { "<leader>fh", function() require("fzf-lua").help_tags() end, desc = "Help Tags" },
+        { "<leader>fb", function() focus_edit_window(); require("fzf-lua").buffers() end, desc = "Buffers" },
+        { "<leader>fh", function() focus_edit_window(); require("fzf-lua").help_tags() end, desc = "Help Tags" },
         {
             "<leader>fs",
             function()
-                require("fzf-lua").grep(with_neotree_directory({ search = vim.fn.input("Grep For > ") }))
+                local opts = with_neotree_directory({ search = vim.fn.input("Grep For > ") })
+                focus_edit_window()
+                require("fzf-lua").grep(opts)
             end,
             desc = "FZF grep with input",
         },
