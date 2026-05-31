6dfc2d9aa78078a15e40b99d3cebe7cd3142facc
Khoa Vu <khoavu.its@gmail.com>
2026-06-01 00:50:43 +0700

    fix(nvim): restore terminal mode after navigation

--- nvim/lua/plugins/tmux-navigator.lua
+++ nvim/lua/plugins/tmux-navigator.lua
@@ -2,14 +2,32 @@ return {
     "christoomey/vim-tmux-navigator",
     event = "VeryLazy",
     config = function()
-        vim.keymap.set('n', '<C-h>', '<cmd>TmuxNavigateLeft<CR>', { silent = true })
-        vim.keymap.set('n', '<C-j>', '<cmd>TmuxNavigateDown<CR>', { silent = true })
-        vim.keymap.set('n', '<C-k>', '<cmd>TmuxNavigateUp<CR>', { silent = true })
-        vim.keymap.set('n', '<C-l>', '<cmd>TmuxNavigateRight<CR>', { silent = true })
-        -- Terminal mode navigation (escape terminal first, then navigate)
-        vim.keymap.set('t', '<C-h>', '<C-\\><C-n><cmd>TmuxNavigateLeft<CR>', { silent = true })
-        vim.keymap.set('t', '<C-j>', '<C-\\><C-n><cmd>TmuxNavigateDown<CR>', { silent = true })
-        vim.keymap.set('t', '<C-k>', '<C-\\><C-n><cmd>TmuxNavigateUp<CR>', { silent = true })
-        vim.keymap.set('t', '<C-l>', '<C-\\><C-n><cmd>TmuxNavigateRight<CR>', { silent = true })
+        local function navigate(command)
+            vim.cmd(command)
+            vim.schedule(function()
+                if vim.bo.buftype == "terminal" then
+                    vim.cmd("normal! G")
+                    vim.cmd.startinsert()
+                end
+            end)
+        end
+
+        local function terminal_navigate(command)
+            vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<C-\\><C-n>", true, false, true), "n", false)
+            vim.schedule(function()
+                navigate(command)
+            end)
+        end
+
+        vim.keymap.set('n', '<C-h>', function() navigate("TmuxNavigateLeft") end, { silent = true })
+        vim.keymap.set('n', '<C-j>', function() navigate("TmuxNavigateDown") end, { silent = true })
+        vim.keymap.set('n', '<C-k>', function() navigate("TmuxNavigateUp") end, { silent = true })
+        vim.keymap.set('n', '<C-l>', function() navigate("TmuxNavigateRight") end, { silent = true })
+
+        -- Terminal mode navigation: escape first, move, then resume terminal input on terminal windows.
+        vim.keymap.set('t', '<C-h>', function() terminal_navigate("TmuxNavigateLeft") end, { silent = true })
+        vim.keymap.set('t', '<C-j>', function() terminal_navigate("TmuxNavigateDown") end, { silent = true })
+        vim.keymap.set('t', '<C-k>', function() terminal_navigate("TmuxNavigateUp") end, { silent = true })
+        vim.keymap.set('t', '<C-l>', function() terminal_navigate("TmuxNavigateRight") end, { silent = true })
     end
 }
